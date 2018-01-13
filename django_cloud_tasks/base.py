dd1d46f3cc21c0186a0268000861ffca018eed61
George Lubaretsi <georgelubaretsi@gmail.com>
2018-01-13 06:52:08 +0400

    Add task scheduling retries. Cloud Tasks is randomly throwing "service unavailable" from time to time

--- django_cloud_tasks/base.py
+++ django_cloud_tasks/base.py
@@ -1,13 +1,42 @@
 import base64
 import json
+import logging
+import time
 
 from django.conf import settings
 
-from .connection import connection
 from .apps import DCTConfig
-import logging
+from .connection import connection
 
 logger = logging.getLogger(__name__)
+
+
+def retry(retry_limit, retry_interval):
+    """
+    Decorator for retrying task scheduling
+    """
+
+    def decorator(f):
+        def wrapper():
+            attempts_left = retry_limit
+            error = None
+            while attempts_left > 1:
+                try:
+                    return f()
+                except Exception as e:
+                    error = e
+                    msg = 'Task scheduling failed. Reason: {0}. Retrying...'.format(str(e))
+                    logger.warning(msg)
+                    time.sleep(retry_interval)
+                    attempts_left -= 1
+
+            # Limit exhausted
+            error.args = ('Task scheduling limit exhausted',) + error.args
+            raise error
+
+        return wrapper
+
+    return decorator
 
 
 def batch_callback_logger(id, message, exception):
@@ -17,10 +46,12 @@ def batch_callback_logger(id, message, exception):
         raise Exception(decoded['error']['message'])
 
 
-def batch_execute(tasks):
+def batch_execute(tasks, retry_limit=30, retry_interval=3):
     """
     Executes tasks in batch
     :param tasks: list of CloudTaskWrapper objects
+    :param retry_limit: How many times task scheduling will be attempted
+    :param retry_interval: Interval between task scheduling attempts in seconds
     """
     if len(tasks) >= 1000:
         raise Exception('Maximum number of tasks in batch cannot exceed 1000')
@@ -28,7 +59,11 @@ def batch_execute(tasks):
     batch = client.new_batch_http_request()
     for t in tasks:
         batch.add(t.create_cloud_task(), callback=batch_callback_logger)
-    batch.execute()
+
+    if not retry_limit:
+        return batch.execute()
+    else:
+        return retry(retry_limit=retry_limit, retry_interval=retry_interval)(batch.execute)()
 
 
 class BaseTask(object):
@@ -50,11 +85,16 @@ class CloudTaskWrapper(object):
             con = connection
         self._connection = con
 
-    def execute(self):
+    def execute(self, retry_limit=10, retry_interval=5):
         """
         Enqueue cloud task and send for execution
+        :param retry_limit: How many times task scheduling will be attempted
+        :param retry_interval: Interval between task scheduling attempts in seconds
         """
-        return self.create_cloud_task().execute()
+        if not retry_limit:
+            return self.create_cloud_task().execute()
+        else:
+            return retry(retry_limit=retry_limit, retry_interval=retry_interval)(self.create_cloud_task().execute)()
 
     def run(self):
         """
@@ -93,4 +133,3 @@ class CloudTaskWrapper(object):
         task = self._connection.tasks_endpoint.create(parent=self._cloud_task_queue_name, body=body)
 
         return task
-
