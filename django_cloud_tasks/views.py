406fb0f3cae5089e12cf84ba2dc5890f2d08917f
George Lubaretsi <georgelubaretsi@gmail.com>
2018-01-27 04:22:34 +0400

    Pass task payload to  logger keyword argument

--- django_cloud_tasks/views.py
+++ django_cloud_tasks/views.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 @csrf_exempt
 def run_task(request):
     body = json.loads(request.body.decode('utf-8'))
-    request_headers = str(request.META)
+    request_headers = request.META
     try:
         internal_task_name = body['internal_task_name']
         data = body.get('data', dict())
@@ -19,9 +19,12 @@ def run_task(request):
         cloud_request = CloudTaskRequest.from_cloud_request(request)
         func.run(request=cloud_request, **data) if data else func.run(request=cloud_request)
     except:
-        message = 'Task execution failed. \nRequest body: {0} \nRequest headers: {1}'
+        message = 'Task execution failed'
         logger.exception(
-            message.format(str(body), str(request_headers))
+            message, extra={
+                'taskPayload': body,
+                'taskRequestHeaders': dict(request_headers)
+            }
         )
         return JsonResponse({'status': 'error'}, status=500)
 
