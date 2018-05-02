ba5cd5c1f9f643a542c08ea295a752a48ed31781
George Lubaretsi <georgelubaretsi@gmail.com>
2018-05-02 11:30:16 +0400

    Add more encoding options to JSON encoder

--- django_cloud_tasks/base.py
+++ django_cloud_tasks/base.py
@@ -1,9 +1,10 @@
 import base64
 import json
+import decimal
 import logging
 import time
 import uuid
-from uuid import UUID
+import datetime
 
 from .apps import DCTConfig
 from .connection import connection
@@ -11,11 +12,58 @@ from .connection import connection
 logger = logging.getLogger(__name__)
 
 
+def _get_duration_components(duration):
+    days = duration.days
+    seconds = duration.seconds
+    microseconds = duration.microseconds
+
+    minutes = seconds // 60
+    seconds = seconds % 60
+
+    hours = minutes // 60
+    minutes = minutes % 60
+
+    return days, hours, minutes, seconds, microseconds
+
+
+def _duration_iso_string(duration):
+    if duration < datetime.timedelta(0):
+        sign = '-'
+        duration *= -1
+    else:
+        sign = ''
+
+    days, hours, minutes, seconds, microseconds = _get_duration_components(duration)
+    ms = '.{:06d}'.format(microseconds) if microseconds else ""
+    return '{}P{}DT{:02d}H{:02d}M{:02d}{}S'.format(sign, days, hours, minutes, seconds, ms)
+
+
 class ComplexEncoder(json.JSONEncoder):
     def default(self, obj):
-        if isinstance(obj, UUID):
+        if isinstance(obj, datetime.datetime):
+            r = obj.isoformat()
+            if obj.microsecond:
+                r = r[:23] + r[26:]
+            if r.endswith('+00:00'):
+                r = r[:-6] + 'Z'
+            return r
+        elif isinstance(obj, datetime.date):
+            return obj.isoformat()
+        elif isinstance(obj, datetime.time):
+            if obj.utcoffset() is not None:
+                raise ValueError("JSON can't represent timezone-aware times.")
+            r = obj.isoformat()
+            if obj.microsecond:
+                r = r[:12]
+            return r
+        elif isinstance(obj, datetime.timedelta):
+            return _duration_iso_string(obj)
+        elif isinstance(obj, decimal.Decimal):
+            return str(obj)
+        elif isinstance(obj, uuid.UUID):
             return obj.hex
-        return json.JSONEncoder.default(self, obj)
+        else:
+            return super().default(obj)
 
 
 def retry(retry_limit, retry_interval):
