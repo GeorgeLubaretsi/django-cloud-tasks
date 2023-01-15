a1621497aa603737073c4c48ee39d1817163a42f
François Rossigneux <francois.rossigneux@gmail.com>
2023-01-15 21:04:43 +0100

    Fix imports

--- django_cloud_tasks/urls.py
+++ django_cloud_tasks/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.run_task, name='run_task'),
+    path('', views.run_task, name='run_task'),
 ]
